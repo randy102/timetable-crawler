168f887b747ac20b3fe4c2f2b03a910aa5f05424
randytran <welldey102@gmail.com>
2021-12-21 10:37:23 +0700

    Fix: remove timeout and increase ttl

--- helper/cache.js
+++ helper/cache.js
@@ -1,6 +1,6 @@
 const NodeCache = require("node-cache");
 
-const subjectCache = new NodeCache({ stdTTL: 60 * 60 });
+const subjectCache = new NodeCache({ stdTTL: 60 * 60 * 24 });
 
 module.exports = {
     subjectCache
